bd8b7b716ce6c6a58430a31c8fbd21abfcbd707c
Vinicius dos Passos da Silva <viniciuspassos030@gmail.com>
2025-03-05 21:21:07 -0300

    Adição de Arquivos
    Arquivo referente a suporte

--- Config.py
+++ Config.py
@@ -1,7 +1,16 @@
+import mysql.connector
+
 MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
 MYSQL_PASSWORD = ""
-MYSQL_DATABASE = "ViniciusSilva_DB"
+MYSQL_DATABASE = "ForneInjet_SA"
 
+def get_connection():
+    return mysql.connector.connect(
+        host = MYSQL_HOST,
+        user = MYSQL_USER,
+        password = MYSQL_PASSWORD,
+        database = MYSQL_DATABASE
+    )
 # Codigo para executar o projeto:
-# pip install mysql-connector-python
+# pip install mysql-connector-python    
