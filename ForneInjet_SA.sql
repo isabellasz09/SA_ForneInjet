2a2a00254de4a8ab8078c02ff4a9ac2e962eaff2
Vinicius dos Passos da Silva <viniciuspassos030@gmail.com>
2025-03-05 21:25:06 -0300

    Correção de Arquivo

--- ForneInjet_SA.sql
+++ ForneInjet_SA.sql
@@ -1,11 +1,38 @@
-create database ViniciusSilva_DB
+create database ForneInjet_SA
 
-create table usuario(
-	idusuario int not null auto_increment,
-    nome text,
+create table Funcionario(
+	idFuncionario int not null auto_increment,
+    nome_funcionario text,
+    departamento text,
     telefone text,
     email text,
-    usuario text,
-    senha text,
-    primary key (idusuario)    
+    data_admissao text,
+    situacao text,
+    permicao text,
+    primary key (idFuncionario)    
+);
+
+create table Produto(
+	idMaquinas int not null auto_increment,
+    tipo text,
+    marca text,
+    Capacidade_de_Injeção text,
+    Força_de_Fechamento text,
+    Tipo_de_Controle text,
+    Preço_Médio_USD text,
+    Preço_Médio_BRL text,
+    fornecedor text,
+    observacoes text,
+    primary key (idMaquinas)    
+);
+
+create table Fornecedor(
+	idFornecedor int not null auto_increment,
+    nome_fornecedor text,  
+    endereco text,
+    telefone text,
+    email text,
+    contato_principal text,
+    website text,
+    primary key (idFornecedor)    
 );
